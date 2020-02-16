24b3f7890d2c2ed1242c9e941ac2b1a7fb66f242
Rezeda Khairullina <deserto.raposa@yandex.ru>
2020-02-17 00:18:19 +0300

    CSS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "./style.css";
 console.log('Hello, diplomaaaa!');
 
 const numbers = [2, 3, 5];
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 
 const path = require('path');
 // подключаем path к конфигу вебпак
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+ // подключили к проекту плагин
 
 module.exports = {
     entry: { main: './src/index.js' },
@@ -13,8 +15,15 @@ module.exports = {
             test: /\.js$/, // регулярное выражение, которое ищет все js файлы
             use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
             exclude: /node_modules/ // исключает папку node_modules
+        },
+        {
+            test: /\.css$/, // применять это правило только к CSS-файлам
+            use: [MiniCssExtractPlugin.loader, 'css-loader'] // к этим файлам нужно применить пакеты, которые мы уже установили
         }
         ]
-    }
+    },
+    plugins: [ 
+        new MiniCssExtractPlugin({filename: 'style.css'})
+        ]
 }
 
