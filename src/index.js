5931f0e0ac36b5b969a8746b99b99d77e2c7f005
Rezeda Khairullina <deserto.raposa@yandex.ru>
2020-02-16 22:12:56 +0300

    installed babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-let someStr = 'Изменил, сохранил, собрал';
+console.log('Hello, diplomaaaa!');
+
+const numbers = [2, 3, 5];
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers); // 4, 6, 10
