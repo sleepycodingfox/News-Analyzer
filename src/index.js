85cd24be9e0e5d64bbe39739e122165a8079ac98
Rezeda Khairullina <deserto.raposa@yandex.ru>
2020-02-17 01:44:04 +0300

    added hashing, postCSS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./style.css";
-console.log('Hello, diplomaaaa!');
+//console.log('Hello, diplomaaaa!');
 
 const numbers = [2, 3, 5];
 const doubledNumbers = numbers.map(number => number * 2);
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,13 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 // подключили к проекту плагин
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
 
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        filename: '[name].[chunkhash].js'
     },
     module: {
         rules: [{ // тут описываются правила
@@ -19,19 +20,22 @@ module.exports = {
         },
         {
             test: /\.css$/, // применять это правило только к CSS-файлам
-            use: [MiniCssExtractPlugin.loader, 'css-loader'] // к этим файлам нужно применить пакеты, которые мы уже установили
+            use: [MiniCssExtractPlugin.loader, 'css-loader','postcss-loader'] // к этим файлам нужно применить пакеты, которые мы уже установили
         }
         ]
     },
     plugins: [
-        new MiniCssExtractPlugin({ filename: 'style.css' }),
+        new MiniCssExtractPlugin({
+             filename: 'style.[contenthash].css'
+             }),
         new HtmlWebpackPlugin({
             // Означает, что:
             inject: false, // стили НЕ нужно прописывать внутри тегов
-            hash: true, // для страницы нужно считать хеш
             template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
             filename: 'index.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
-          })
+          }),
+
+          new WebpackMd5Hash()
     ]
 }
 
