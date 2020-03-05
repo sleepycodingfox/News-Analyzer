9454637f032ee5c1cbb3d5a88e498fb6559ffe9f
Rezeda Khairullina <deserto.raposa@yandex.ru>
2020-03-05 06:42:02 +0300

    Added first html and css blocks version

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import "./style.css";
-//console.log('Hello, diplomaaaa!');
-
-const numbers = [2, 3, 5];
-const doubledNumbers = numbers.map(number => number * 2);
-
-console.log(doubledNumbers); // 4, 6, 10
