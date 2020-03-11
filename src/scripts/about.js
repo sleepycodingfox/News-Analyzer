cbe42a821e9e8a6a8374c5af628ee6d851f02c27
Rezeda Khairullina <deserto.raposa@yandex.ru>
2020-03-11 13:10:42 +0300

    Added Swiper plugin

--- src/scripts/about.js
+++ src/scripts/about.js
@@ -1,2 +1,27 @@
 import '../blocks/about.css';
 import '../images/group.ico';
+import '../../node_modules/swiper/js/swiper.min.js';
+import Swiper from 'swiper';
+
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
